779dc21efa2d33d094d79f092f5c95da7ab48edd
unknown <Suporte@ACESSO-06.acesso.edu>
2022-10-13 11:50:56 -0300

    Atualização parte 1 Responsivo 600

--- recarga/urls.py
+++ recarga/urls.py
@@ -5,6 +5,3 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:cliente_id>', views.exibir_saldo, name='exibir_saldo'),
 ]
-
-handler404 = "recarga.views.handler404"
-handler500 = "recarga.views.handler500"
--- recarga/views.py
+++ recarga/views.py
@@ -12,11 +12,3 @@ def exibir_saldo(request, cliente_id):
     cliente = Cliente.objects.get(cartao=cliente_id)
     return render(request, 'recarga/exibir_saldo.html',
                   {'cliente': cliente})
-
-
-def handler404(request, exception):
-    return render(request, 'recarga/404.html', status=404)
-
-
-def handler500(request):
-    return render(request, 'recarga/500.html', status=500)
