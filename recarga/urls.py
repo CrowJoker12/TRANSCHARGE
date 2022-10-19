2d52d73971ec213ccd1939b4d56da48d5b96b468
unknown <Suporte@ACESSO-06.acesso.edu>
2022-10-19 09:39:56 -0300

    Página de Erro funcional

--- recarga/urls.py
+++ recarga/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:cliente_id>', views.exibir_saldo, name='exibir_saldo'),
+    path('<str:cliente_id>', views.erro500, name='erro'),
 ]
--- recarga/views.py
+++ recarga/views.py
@@ -12,3 +12,7 @@ def exibir_saldo(request, cliente_id):
     cliente = Cliente.objects.get(cartao=cliente_id)
     return render(request, 'recarga/exibir_saldo.html',
                   {'cliente': cliente})
+
+
+def erro500(request):
+    return render(request, 'recarga/500.html')
